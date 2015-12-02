d7bf991244e3b279480879eb65b6063e7cbdb32c
Nick Cox <nick@nickcox.me>
2015-12-01 22:56:42 -0800

    initial commit; working

---
+++ github_followers.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type User struct {
+	Login string
+}
+
+func retrieveGithubAPIData() []byte {
+	const githubURL string = "https://api.github.com/users/"
+
+	url := githubURL + os.Args[1] + "/followers"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	body, err2 := ioutil.ReadAll(resp.Body)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	return body
+}
+
+func main() {
+	var users []User
+
+	err := json.Unmarshal(retrieveGithubAPIData(), &users)
+	if err != nil {
+		panic(err)
+	}
+	for user := range users {
+		fmt.Println(users[user].Login)
+	}
+}
